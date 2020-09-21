d545b477113ae55340651a508894cbf039ba3c94
Shiwei Zhang <ylxdzsw@gmail.com>
2020-09-21 21:52:30 +0800

    .

--- pdf_tools/crop.jl
+++ pdf_tools/crop.jl
@@ -3,12 +3,14 @@ using Fire
 
 """
 Crop a pdf file to fit iPad screen (3:4).
-Optionally trim the margin left, bottom, right, top in %
+Optionally trim the margin left, bottom, right, top in %.
+If side_offset is set, it will also adjust for two side pdfs.
 """
-@main function crop(file; trim::Vector{f32}=f32[5,5,5,5])
+@main function crop(file; trim::Vector{f32}=f32[5,5,5,5], side_offset::f32=0.)
     size = read(`sh -c "pdfinfo $file | awk '/Page size/{print \$3,\$5}'"`, String)
     w, h = parse.(f32, split(size))
     l, b, r, t = trim .* [w, h, w, h] ./ 100
+    side_offset *= w ./ 100
 
     if (w - l - r) / (h - b - t) < .75
         d = (h - b - t) - (w - l - r) / .75
@@ -22,11 +24,11 @@ Optionally trim the margin left, bottom, right, top in %
 
     open("main.tex", "w") do f
         f << """
-            \\documentclass{article}
+            \\documentclass[twoside]{article}
             \\usepackage{pdfpages}
             \\usepackage[paperwidth=$(w - l - r)bp,paperheight=$(h - b - t)bp]{geometry}
             \\begin{document}
-            \\includepdf[pages=-, scale=1, trim=$(l)bp $(b)bp $(r)bp $(t)bp]{$file}
+            \\includepdf[pages=-, scale=1, offset=$(side_offset)bp 0, trim=$(l)bp $(b)bp $(r)bp $(t)bp]{$file}
             \\end{document}
         """
     end
